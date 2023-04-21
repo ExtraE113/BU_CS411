82e458556bca739b2d3c85b26eb5e7a3c76b8960
Jiang Cheng Liu <jiangcl@bu.edu>
2023-04-21 12:30:05 -0400

    stuff

---
+++ podcasts/podcast_app/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import Podcast, Episode
+# Register your models here.
+admin.site.register(Podcast)
+admin.site.register(Episode)
--- podcasts/settings.py
+++ podcasts/settings.py
@@ -37,7 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'podcast_app',
+    'allauth', 
+    'allauth.account', 
+    'allauth.socialaccount', 
+    'allauth.socialaccount.providers.google'
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -122,3 +129,36 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTHENTICATION_BACKENDS = [
+
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
+
+SITE_ID = 1
+
+SOCIALACCOUNT_PROVIDERS = {
+    'google': {
+        'SCOPE': ['profile', 'email'],
+        'AUTH_PARAMS': {'access_type': 'online'},
+        'APP': {
+            'client_id': '708378597417-0v12l611cch6jioq9oj05b354oe65c50.apps.googleusercontent.com',
+            'secret': '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',
+            'key': ''
+        }
+    }
+}
+
+ACCOUNT_AUTHENTICATION_METHOD = 'email'
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_EMAIL_VERIFICATION = 'mandatory'  # or 'optional' or 'none'
+ACCOUNT_USERNAME_REQUIRED = False
+ACCOUNT_SIGNUP_PASSWORD_ENTER_TWICE = True
+ACCOUNT_SESSION_REMEMBER = True
+
+LOGIN_REDIRECT_URL = 'home'
+
