91febf7b51f7b5ce621faee66769770e06106440
Ivan Yang <yangivan0614@gmail.com>
2023-04-29 22:32:47 -0400

    implemented way to fetch data from django backend/created podcast cards

--- frontend/src/components/scripts/Dashboard.js
+++ frontend/src/components/scripts/Dashboard.js
@@ -4,29 +4,59 @@ import { doc, setDoc } from "firebase/firestore";
 import React, { useEffect, useState } from 'react';
 import { getAuth } from "firebase/auth";
 import { getFirestore, getDoc } from "firebase/firestore";
+import PodcastCards from "./PodcastCards";
+// import axios from "axios";
 
 function Dashboard() {
     const [podcasts, setPodcasts] = useState(null);
     const [user, setUser] = useState([]);
 
     useEffect(() => {
-        const auth = getAuth();
-        const loggedUser = auth.onAuthStateChanged(async (user) => {
-        if (user) {
-            setUser(user);
-            setDoc(doc(db, "users", user.uid), {
-                email: user.email
-            });
-        } else {
-            setUser(null);
-        }
-        });
-        return () => loggedUser();
-    }, []);
+        fetch("http://127.0.0.1:8000/podcasts/")
+          .then(response => response.json())
+          .then(data => setPodcasts(data))
+          .catch(error => console.log(error));
+      }, []);
+
+    //Axios has some issues as it returns a HTML static file instead of JSON format
+//   useEffect(() => {
+//     axios
+//       .get("http://127.0.0.1:8000/podcasts/?format=api")
+//       .then(response => {
+//         setPodcasts(response.JSON);
+//       })
+//       .catch(error => {
+//         console.log(error);
+//       });
+//   }, []);
+
+    // useEffect(() => {
+    //     const auth = getAuth();
+    //     const loggedUser = auth.onAuthStateChanged(async (user) => {
+    //     if (user) {
+    //         setUser(user);
+    //         setDoc(doc(db, "users", user.uid), {
+    //             email: user.email
+    //         });
+    //     } else {
+    //         setUser(null);
+    //     }
+    //     });
+    //     return () => loggedUser();
+    // }, []);
+
+    if (podcasts === null) {
+        return <div>Loading...</div>;
+    }
 
     return(
         <section>
             <h1 className = {styles.favorites}>My Favorites</h1>
+            <div>
+                {podcasts && podcasts.map(podcast => (
+                    <PodcastCards data={podcast} />
+                ))}
+            </div>
             <button className = {styles.addPodcastBtn}>Add Podcast</button>
         </section>
     );
