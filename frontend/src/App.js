63810f43bc79042b7cc2e657a24eca14efbbe6de
Ivan Yang <yangivan0614@gmail.com>
2023-05-05 03:16:41 -0400

    attempting oauth login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,19 @@
 import './App.css';
-import { BrowserRouter,Routes,Route,Link } from "react-router-dom";
-import NavBar from './components/scripts/Navbar';
-import Login from './components/scripts/Login';
-import Dashboard from './components/scripts/Dashboard';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import {Login} from "../src/components/scripts/Login";
+import {Home} from "../src/components/scripts/Home";
+import {Navigation} from "../src/components/scripts/Navigations";
+import {Logout} from "../src/components/scripts/Logout";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element = {<Login />} />
-        <Route path="/home" element = {[<NavBar />, <Dashboard />] } />
-      </Routes>
-    </BrowserRouter>
-  );
+    return <BrowserRouter>
+    <Navigation></Navigation>
+        <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/login" element={<Login/>}/>
+            <Route path="/logout" element={<Logout/>}/>
+        </Routes>
+    </BrowserRouter>;
 }
 
 export default App;
--- frontend/src/components/scripts/Login.js
+++ frontend/src/components/scripts/Login.js
@@ -1,60 +1,48 @@
-import styles from "../styles/Login.module.css";
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, getUserByEmail } from "firebase/auth";
-import { auth, db } from "../../firebase";
+import axios from "axios";
+import {Navigate} from "react-router-dom";
+import {useState} from "react";
+import GoogleLogin from "react-google-login";
+import {gapi} from "gapi-script";
+import {useEffect} from "react";
+import 'bootstrap/dist/css/bootstrap.css';
 
-function Login() {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const navigate = useNavigate();
+export const Login = () => {
+    const client_id = '708378597417-g4gp2dmet2rarqs4bb6djof4e3kfnu72.apps.googleusercontent.com';
 
-    const signIn = e => {
-        e.preventDefault();
+    const onSuccess = async (res) => {
+        console.log('succzddgsfgess:', res.accessToken);
+        const user = {
+            "grant_type":"convert_token",
+            "client_id": client_id,
+            "client_secret": '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',
+            "backend":"google-oauth2",
+            "token": res.accessToken
+        };
+        //console.log(user)
+        const {data} = await axios.post('http://127.0.0.1:8000/auth/convert-token', user ,{headers: {
+            'Content-Type': 'application/json'
+        }}, {withCredentials: true});
 
-        const auth = getAuth();
-        signInWithEmailAndPassword(auth, email, password)
-        .then(auth => {
-            navigate("/home");
-        })
-        .catch(error => alert(error.message))
+        //console.log(data, data['access_token'])
+        axios.defaults.headers.common['Authorization'] = `Bearer ${data['access_token']}`;
+        localStorage.clear();
+        localStorage.setItem('access_token', data.access_token);
+        localStorage.setItem('refresh_token', data.refresh_token);
+        window.location.href = '/'
+
+        // const onFailure = (err) => {
+        //     console.log('failed:', err);
+        // };
     }
-
-    const createAccount = e => {
-        e.preventDefault();
-
-        const auth = getAuth();
-        createUserWithEmailAndPassword(auth, email, password)
-        .then((auth) => {
-            if (auth) {
-                navigate("/home");
-            }
-        })
-        .catch(error => alert(error.message))
-    }
-
-    return (
-        <section className = {styles.login}>
-            <div>
-                <h1>Podcast</h1>
-                <form>
-                    <div className = {styles.emailInput}>
-                        <h4 className= {styles.logo3}> Email</h4>
-                        <input type = "text" value = {email} onChange = {e => {setEmail(e.target.value)}} className = {styles.email}/>
-                    </div>
-                    <div className = {styles.passwordInput}>
-                        <h4 className= {styles.logo3}> Password</h4>
-                        <input type = "password" value = {password} onChange = {e => {setPassword(e.target.value)}} className = {styles.password} />
-                    </div>
-
-                    <button type = "submit" onClick = {signIn} className = {styles.signInBtn}>Sign In</button>
-                </form>
-
-                <p className = {styles.message}>Don't have an account? Create one here!</p>
-                <button className = {styles.createAccBtn} onClick = {createAccount}>Create your account</button>
-            </div>
-        </section>
-    );
+    
+    return(
+        <div className="Auth-form-container">
+          <GoogleLogin
+            clientId={client_id}
+            buttonText="Sign in with Google"
+            onSuccess={onSuccess}
+            cookiePolicy={'single_host_origin'}
+            />
+        </div>
+    )
 }
-
-export default Login;
